047b0f804ef763c71351ea2dd45cbc0bef5f7516
DanSViana <sousadaniel208@gmail.com>
2024-08-07 17:32:11 -0300

    Versão 1.0

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+Crie um programa que receba do usuário o seu nome e a idade, e mostre os dados na tela. Ao terminar, envie o arquivo .py.
+"""
+
+# declaração de dados
+nome: str = input('Informe seu nome: ')
+idade: int = input('Informe a idade: ')
+
+#saída de dados 
+print('Nome: ' + nome + '.')
+print('Idade: ' + str(idade) + '.')
